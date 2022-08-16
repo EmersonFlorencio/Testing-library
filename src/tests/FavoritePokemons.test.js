5cabc112190cdda243120c09f331fd0c1a14fc1d
Emerson Florencio <emersonflorencio@hotmail.com>
2022-08-16 15:28:36 -0300

    Resolução do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePkemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o Componente Favorite Pokemons', () => {
+  test('Se a mensagem "no favorite pokemons found" aparece na tela', () => {
+    renderWithRouter(<FavoritePkemons />);
+
+    const favoriteFound = screen.getByText(/no favorite pokemon found/i);
+
+    expect(favoriteFound).toBeInTheDocument();
+  });
+});
