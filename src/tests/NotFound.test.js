5eb092a084b53f4017310432a5104da76d8345b3
Emerson Florencio <emersonflorencio@hotmail.com>
2022-08-16 15:33:02 -0300

    Resolução do requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o Componete NotFound', () => {
+  test('Se a pagina contem um H2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', { name: /Page requested not found/i,
+      level: 2 });
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Se mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const altImg = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+
+    expect(altImg).toHaveAttribute('src', imgUrl);
+  });
+});
